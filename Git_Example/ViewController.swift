0e62e320e3b385ec4be84dfb49637d456ab1d834
Alexander Kolovatov <a.kolovatov@gmail.com>
2019-06-25 12:19:22 +0600

    initial commit

---
+++ Git_Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git_Example
+//
+//  Created by Alex Kolovatov on 25/06/2019.
+//  Copyright © 2019 Alex Kolovatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
